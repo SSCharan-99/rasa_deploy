d7dc3598960240c51d7a48a808da359738c9b309
SSCharan-99 <saisricharan99@msitprogram.net>
2021-06-02 22:52:50 +0530

    fir commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+import { Home } from './Home';
+import { About } from './About';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
+}
+
+export default App;
