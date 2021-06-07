f20dd066df862e9b26661a6e3e781333e6b20c09
SSCharan-99 <saisricharan99@msitprogram.net>
2021-06-07 11:47:08 +0530

    unk

--- frontend/server.js
+++ frontend/server.js
@@ -8,8 +8,8 @@ server.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + '/index.html');
  });
